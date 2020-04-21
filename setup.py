4cb9b9c34186c5c3895d667c7423c4dc4fe7e035
felytic <felytic@gmail.com>
2020-04-21 20:23:43 +0300

    Update v0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='vincenty-cuda-nns',
-    version='0.1.6',
+    version='0.2.0',
     author='Serhii Hulko',
     author_email='felytic@gmail.com',
     description='Nearest neighbor search on Earth\'s surface with a GPU',
